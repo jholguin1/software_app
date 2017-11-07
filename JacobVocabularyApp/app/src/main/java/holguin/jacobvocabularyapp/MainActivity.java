c23d2f69264c802ecce6ea575c3d7f11643c1374
Jacob Holguin <jholguin1@cps.edu>
2017-11-07 13:24:17 -0600

    Get the radioButton checked Method

--- JacobVocabularyApp/app/src/main/java/holguin/jacobvocabularyapp/MainActivity.java
+++ JacobVocabularyApp/app/src/main/java/holguin/jacobvocabularyapp/MainActivity.java
@@ -1,8 +1,13 @@
 package holguin.jacobvocabularyapp;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
     private Button initalizeBtn;
     private Button findViewBtn;
     private Button methodBtn;
-    private Button onCreate;
-    private Button contentViewBtn;
-    private Button onClickListenerBtn;
-    private Button onClick;
-    private Button intentBtn;
-    private Button startActvityBtn;
+    private RadioButton onCreate;
+    private RadioButton contentViewBtn;
+    private RadioGroup rG;
+//    private Button onClickListenerBtn;
+//    private Button onClick;
+//    private Button intentBtn;
+//    private Button startActvityBtn;
 
 
 
@@ -31,11 +37,32 @@ public class MainActivity extends AppCompatActivity {
         initalizeBtn = (Button)findViewById(R.id.intializeBtn);
         findViewBtn  = (Button)findViewById(R.id.findViewBtn);
         methodBtn = (Button)findViewById(R.id.methodBtn);
-        onCreate = (Button)findViewById(R.id.onCreateBtn);
-        contentViewBtn = (Button)findViewById(R.id.setContentBtn);
-        onClickListenerBtn = (Button)findViewById(R.id.clickListenerBtn);
-        onClick = (Button)findViewById(R.id.onClickBtn);
-        intentBtn = (Button)findViewById(R.id.intentBtn);
-        startActvityBtn = (Button)findViewById(R.id.startActivityBtn);
+        onCreate = (RadioButton)findViewById(R.id.onCreateRBtn);
+        contentViewBtn = (RadioButton)findViewById(R.id.contentViewRBtn);
+        rG = (RadioGroup)findViewById(R.id.radioGroup);
+//        onClickListenerBtn = (Button)findViewById(R.id.clickListenerBtn);
+//        onClick = (Button)findViewById(R.id.onClickBtn);
+//        intentBtn = (Button)findViewById(R.id.intentBtn);
+//        startActvityBtn = (Button)findViewById(R.id.startActivityBtn);
+
+        activtyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                if(onCreate.isChecked()){
+
+                }
+                if(contentViewBtn.isChecked()){
+
+                }
+            }
+        });
+
+
     }
 }
--- JacobVocabularyApp/app/src/main/java/holguin/jacobvocabularyapp/setContentViewActivity.java
+++ JacobVocabularyApp/app/src/main/java/holguin/jacobvocabularyapp/setContentViewActivity.java
@@ -4,12 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 public class setContentViewActivity extends AppCompatActivity {
-
-    private RadioButton setContentViewBtn;
-    private RadioButton onCreateBtn;
 
 
     @Override
@@ -17,26 +13,9 @@ public class setContentViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_content_view);
 
-        setContentViewBtn = (RadioButton) findViewById(R.id.setContentViewRadioBtn);
-        onCreateBtn = (RadioButton) findViewById(R.id.onCreateRadioBtn);
-
-        public void onRadioButtonClicked(View view) {
-            // Is the button now checked?
-            boolean checked = ((RadioButton) view).isChecked();
-
-            // Check which radio button was clicked
-            switch (view.getId()) {
-                case R.id.setContentViewRadioBtn:
-                    if (checked)
-
-                        break;
-                case R.id.onCreateRadioBtn:
-                    if (checked)
-
-                        break;
-            }
-        }
     }
-
 }
+
+
+
 
