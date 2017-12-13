73b99834aeb1928db490e0ddeb7b3d3921ea5635
Jacob Holguin <jholguin1@cps.edu>
2017-12-13 13:39:02 -0600

    InternetLab

--- InternetAppJacob/app/src/main/java/holguin/internetappjacob/MainActivity.java
+++ InternetAppJacob/app/src/main/java/holguin/internetappjacob/MainActivity.java
@@ -2,12 +2,24 @@ package holguin.internetappjacob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView)findViewById(R.id.titleText);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new WebViewClient());
     }
 }
