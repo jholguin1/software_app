9f4945f81eb1ec0584502c8b14f84158263cf9a5
Jacob Holguin <jholguin1@cps.edu>
2017-12-15 13:24:14 -0600

    Internet app stuff

--- LabInternetAppJacob/app/src/main/java/holguin/labinternetappjacob/MainActivity.java
+++ LabInternetAppJacob/app/src/main/java/holguin/labinternetappjacob/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Spinner spinner;
     TextView title;
@@ -25,41 +26,36 @@ public class MainActivity extends AppCompatActivity{
         webView = (WebView)findViewById(R.id.webView);
         spinner = (Spinner) findViewById(R.id.spinner); // Create an ArrayAdapter using the string array and a default spinner layout
 
-
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new WebViewClient());
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.webpages, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+    }
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(adapterView.getItemAtPosition(i) == "Google.com" ){
-                    webView.getSettings().setJavaScriptEnabled(true);
-                    webView.loadUrl("https://www.google.com");
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if(i == 0){ //identify the current position and compare it to the different positions of the url and then load that url
+                    webView.loadUrl("https://www.yahoo.com/");
                     webView.setWebViewClient(new WebViewClient());
 
-
                 }
-                else if( adapterView.getItemAtPosition(i) == "Twitter.com"){
-                    webView.getSettings().setJavaScriptEnabled(true);
+                else if( i == 1){
                     webView.loadUrl("https://twitter.com");
                     webView.setWebViewClient(new WebViewClient());
 
                 }
                 else {
-                    webView.getSettings().setJavaScriptEnabled(true);
-                    webView.loadUrl("google.cps.edu");
+                    webView.loadUrl("https://portal.id.cps.edu/idp/AuthnEngine#/authn");
                     webView.setWebViewClient(new WebViewClient());
-
                 }
 
-            }
+    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
-            }
-        });
     }
 }
