08511f64950d9781ba50e30dc7dbbe5a1271255e
jholguin1 <jholguin1@cps.edu>
2017-12-17 22:38:28 -0600

    Got the InternetLab to work.

--- LabInternetAppJacob/app/src/main/java/holguin/labinternetappjacob/MainActivity.java
+++ LabInternetAppJacob/app/src/main/java/holguin/labinternetappjacob/MainActivity.java
@@ -23,39 +23,57 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         title = (TextView)findViewById(R.id.textView);
-        webView = (WebView)findViewById(R.id.webView);
         spinner = (Spinner) findViewById(R.id.spinner); // Create an ArrayAdapter using the string array and a default spinner layout
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://www.google.com");
-        webView.setWebViewClient(new WebViewClient());
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.webpages, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        if(i == 0){ //identify the current position and compare it to the different positions of the url and then load that url
-                    webView.loadUrl("https://www.yahoo.com/");
-                    webView.setWebViewClient(new WebViewClient());
+        int iCurrentSelection = spinner.getSelectedItemPosition();
+        if(iCurrentSelection == 1){ //identify the current position and compare it to the different positions of the url and then load that url
+            webView = (WebView)findViewById(R.id.webView);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl("https://www.google.com/");
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+        }
+        else if(iCurrentSelection == 2){
+            webView = (WebView)findViewById(R.id.webView);
+            webView.loadUrl("https://twitter.com");
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
 
+        }
+        else if (iCurrentSelection == 3){
+            webView = (WebView)findViewById(R.id.webView);
+            webView.loadUrl("https://www.yahoo.com/");
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
                 }
-                else if( i == 1){
-                    webView.loadUrl("https://twitter.com");
-                    webView.setWebViewClient(new WebViewClient());
-
-                }
-                else {
-                    webView.loadUrl("https://portal.id.cps.edu/idp/AuthnEngine#/authn");
-                    webView.setWebViewClient(new WebViewClient());
-                }
+            });
+        }
 
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-
+        return;
     }
 }
