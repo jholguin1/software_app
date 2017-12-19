1f0ee717bcdb53ec99af5e57fc15221dea9e4be4
Jacob Holguin <jholguin1@cps.edu>
2017-12-19 13:24:34 -0600

    Location App

--- JacobGPS/app/src/main/java/holguin/jacobgps/MainActivity.java
+++ JacobGPS/app/src/main/java/holguin/jacobgps/MainActivity.java
@@ -1,22 +1,62 @@
 package holguin.jacobgps;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements LocationListener{
+public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
+    LocationListener locationListener;
+    TextView title;
+    TextView latitudeText;
+    TextView longitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        locationManager = (LocationManager).getSystemService(Context.LOCATION_SERVICE);
+        title = (TextView)findViewById(R.id.titleTextView);
+        latitudeText = (TextView)findViewById(R.id.latTextView);
+        longitudeText = (TextView)findViewById(R.id.longTextView);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                
+                latitudeText.setText("Latitude: " + location.getLatitude());
+                longitudeText.setText("Longitude: " + location.getLongitude());
 
 
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
 
     }
+
 }
