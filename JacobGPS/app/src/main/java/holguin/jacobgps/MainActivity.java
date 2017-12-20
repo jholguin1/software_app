860ef12670ab16b6a749ad04967fc36c28a219e1
Jacob Holguin <jholguin1@cps.edu>
2017-12-20 13:22:56 -0600

    Got the Location App to work

--- JacobGPS/app/src/main/java/holguin/jacobgps/MainActivity.java
+++ JacobGPS/app/src/main/java/holguin/jacobgps/MainActivity.java
@@ -1,21 +1,21 @@
 package holguin.jacobgps;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
-    LocationListener locationListener;
     TextView title;
     TextView latitudeText;
     TextView longitudeText;
@@ -25,38 +25,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        title = (TextView)findViewById(R.id.titleTextView);
-        latitudeText = (TextView)findViewById(R.id.latTextView);
-        longitudeText = (TextView)findViewById(R.id.longTextView);
+        title = (TextView) findViewById(R.id.titleTextView);
+        latitudeText = (TextView) findViewById(R.id.latTextView);
+        longitudeText = (TextView) findViewById(R.id.longTextView);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-                
-                latitudeText.setText("Latitude: " + location.getLatitude());
-                longitudeText.setText("Longitude: " + location.getLongitude());
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
 
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    latitudeText.setText("Latitude: " + location.getLatitude());
+                    longitudeText.setText("Longitude: " + location.getLongitude());
+                }
 
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
 
-            }
+                }
 
-            @Override
-            public void onProviderEnabled(String s) {
+                @Override
+                public void onProviderEnabled(String s) {
 
-            }
+                }
 
-            @Override
-            public void onProviderDisabled(String s) {
+                @Override
+                public void onProviderDisabled(String s) {
 
-            }
-        };
+                }
+            });
 
+            return;
+        }
     }
 
 }
