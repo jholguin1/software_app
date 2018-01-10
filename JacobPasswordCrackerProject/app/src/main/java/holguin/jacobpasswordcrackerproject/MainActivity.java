b417d504cf3057b98712d71437e923025cde322e
jholguin1 <jholguin1@cps.edu>
2018-01-09 23:59:16 -0600

    Worked on passwordCracker

--- JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/MainActivity.java
+++ JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/MainActivity.java
@@ -1,13 +1,45 @@
 package holguin.jacobpasswordcrackerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleText;
+    TextView infoText;
+    EditText userInput;
+    CheckBox showPassword;
+    Button checkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleText = (TextView)findViewById(R.id.titleText);
+        infoText = (TextView)findViewById(R.id.infoTextView);
+        userInput = (EditText)findViewById(R.id.userInputText);
+        showPassword = (CheckBox)findViewById(R.id.checkBox);
+        checkBtn = (Button)findViewById(R.id.checkBtn);
+
+        final String password = userInput.getText().toString();
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, userInputActivity.class);
+                intent.putExtra("userPassWord", password);
+                startActivity(intent);
+
+
+            }
+        });
+
     }
 }
--- JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/userInputActivity.java
+++ JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/userInputActivity.java
@@ -1,13 +1,44 @@
 package holguin.jacobpasswordcrackerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class userInputActivity extends AppCompatActivity {
+
+    TextView titleText;
+    TextView pWStrength;
+    TextView pWUsage;
+    ProgressBar progressBar;
+    Button backBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+        //Probably need to come afterschool to get help, specifically about the comparing passwords to some database and/or the machine learning to create a password cracking algorithm
+
+        titleText = (TextView)findViewById(R.id.titleText);
+        pWStrength = (TextView)findViewById(R.id.pWStrength);
+        pWUsage = (TextView)findViewById(R.id.pWUsageText);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        backBtn = (Button)findViewById(R.id.backBtn);
+
+
+        progressBar.setProgress((int) (Math.random() * 100) );
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(userInputActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
