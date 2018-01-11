9a966dc91a5172d2f727eea85440eee9d8ba82bc
Jacob Holguin <jholguin1@cps.edu>
2018-01-11 13:24:36 -0600

    Got some stuff to work

--- JacobGPSAPIClient/app/src/main/java/holguin/jacobgpsapiclient/MainActivity.java
+++ JacobGPSAPIClient/app/src/main/java/holguin/jacobgpsapiclient/MainActivity.java
@@ -1,20 +1,31 @@
 package holguin.jacobgpsapiclient;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient; //import statements
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient; //makes a Fused Location Provider Client manages the location and gives the best location according to our needs.
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //Google Play Services is a proprietary background service and API package for Android devices
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this); //sets this activity's FusedLocationProviderClient
+        textView = (TextView)findViewById(R.id.textView);
 
 
 
@@ -29,16 +41,62 @@ public class MainActivity extends AppCompatActivity {
 
             fusedLocationClient.getLastLocation() //using the getLastLocation Method
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() { //program the OnSuccessListener after the permission check and on the getLastLocation.
-                        @Override                                                   // the OnSuccessListener listens for the success to come out
+                        @Override
+                        // the OnSuccessListener listens for the success to come out
                         public void onSuccess(Location location) { //results of the successes
                             // Got last known location. In some rare situations this can be null.
+
+                            textView.setText();
                             if (location != null) {
                                 // Logic to handle location object
                             }
                         }
                     });
-
             return;
         }
     }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MainActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+
+    }
+
 }
+
