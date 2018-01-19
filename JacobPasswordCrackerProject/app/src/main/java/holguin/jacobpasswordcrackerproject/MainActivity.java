b44f520adfb561c4f7513a7618efc3e3700ec50f
jholguin1 <jholguin1@cps.edu>
2018-01-18 23:27:11 -0600

    Worked on PasswordCracker

--- JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/MainActivity.java
+++ JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/MainActivity.java
@@ -3,9 +3,11 @@ package holguin.jacobpasswordcrackerproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -28,12 +30,33 @@ public class MainActivity extends AppCompatActivity {
         showPassword = (CheckBox)findViewById(R.id.checkBox);
         checkBtn = (Button)findViewById(R.id.checkBtn);
 
-        final String password = userInput.getText().toString();
+
+        showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                int start, end;
+
+                if (!isChecked) {
+                    start = userInput.getSelectionStart();
+                    end = userInput.getSelectionEnd();
+                    userInput.setTransformationMethod(new PasswordTransformationMethod());
+                    userInput.setSelection(start, end);
+                } else {
+                    start = userInput.getSelectionStart();
+                    end = userInput.getSelectionEnd();
+                    userInput.setTransformationMethod(null);
+                    userInput.setSelection(start, end);
+                }
+            }
+        });
+
+        String password = userInput.getText().toString();
 
         checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, userInputActivity.class);
+                String password = userInput.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), userInputActivity.class);
                 intent.putExtra("userPassWord", password);
                 startActivity(intent);
 
--- JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/userInputActivity.java
+++ JacobPasswordCrackerProject/app/src/main/java/holguin/jacobpasswordcrackerproject/userInputActivity.java
@@ -1,6 +1,7 @@
 package holguin.jacobpasswordcrackerproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,15 +31,47 @@ public class userInputActivity extends AppCompatActivity {
         backBtn = (Button)findViewById(R.id.backBtn);
 
 
-        progressBar.setProgress((int) (Math.random() * 100) );
+
+        String password = getIntent().getStringExtra("userPassWord");
+
+        if (password.length() == 0){
+            pWStrength.setText("Password Strength: Invalid");
+            progressBar.setProgress(3);
+            progressBar.getProgressDrawable().setColorFilter(
+                    Color.BLACK, android.graphics.PorterDuff.Mode.SRC_IN);
+
+
+        } else if (password.length() >= 1 && password.length() <= 5){
+            pWStrength.setText("Password Strength: Weak");
+            progressBar.setProgress(1);
+            progressBar.getProgressDrawable().setColorFilter(
+                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+
+
+        } else if (password.length() >= 6 && password.length() <= 9) {
+            pWStrength.setText("Password Strength: Moderate");
+            progressBar.setProgress(2);
+            progressBar.getProgressDrawable().setColorFilter(
+                    Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
+
+
+        } else {
+            pWStrength.setText("Password Strength: Strong");
+            progressBar.setProgress(3);
+            progressBar.getProgressDrawable().setColorFilter(
+                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+
+
+        }
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(userInputActivity.this, MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
 
             }
         });
     }
+
 }
