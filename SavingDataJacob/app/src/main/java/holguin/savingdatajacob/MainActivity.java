cec8b9e3b1f38975c4e561854186e4c76f5d5795
Jacob Holguin <jholguin1@cps.edu>
2018-02-07 13:25:09 -0600

    sdas

---
+++ SavingDataJacob/app/src/main/java/holguin/savingdatajacob/MainActivity.java
@@ -0,0 +1,21 @@
+package holguin.savingdatajacob;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.saved_high_score), newHighScore);
+        editor.commit();
+    }
+}
