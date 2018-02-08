81d882bcc2adfa0cf074058d4969ac7bb023d129
Jacob Holguin <jholguin1@cps.edu>
2018-02-08 13:24:37 -0600

    zippdty

--- SavingDataJacob/app/src/main/java/holguin/savingdatajacob/MainActivity.java
+++ SavingDataJacob/app/src/main/java/holguin/savingdatajacob/MainActivity.java
@@ -4,18 +4,37 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        textView = (TextView)findViewById(R.id.textView);
+
+        int newHighScore = 56;
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
         SharedPreferences.Editor editor = sharedPref.edit();
+
         editor.putInt(getString(R.string.saved_high_score), newHighScore);
+
         editor.commit();
+
+        int defaultValue = getResources().getInteger(R.integer.saved_high_score_default);
+
+        long highScore = sharedPref.getInt(getString(R.string.saved_high_score), defaultValue);
+
+        String result = String.valueOf(highScore);
+
+        textView.setText(result);
+
     }
 }
